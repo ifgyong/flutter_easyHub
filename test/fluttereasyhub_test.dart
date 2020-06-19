b0ee70329a45bbc1bea1ff9967b79bbb8ba1aa5a
ifgyong <fgyong214@gmail.com>
2020-06-19 16:32:48 +0800

    del example

--- test/fluttereasyhub_test.dart
+++ test/fluttereasyhub_test.dart
@@ -1,9 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
 //import 'package:fluttereasyhub/flutter_easyhub.dart';
 
-void main() {
-  test('adds one to input values', () {
-//    EasyHub.show('msg', context);
-  });
-}
+void main() {}
